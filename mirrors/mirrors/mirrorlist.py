e15a52dbde749000bdad35402b0bc452c40a3f3f
Matt Domsch <matt@domsch.com>
2007-09-11 17:46:10 -0500

    mirrorlist.py: greatly speed up SQL queries once again.
    I found the bug in trim() that was exploding the list. With this
    fixed, the SQL query takes <1minute, rather than >15minutes.  Yea!

--- mirrors/mirrors/mirrorlist.py
+++ mirrors/mirrors/mirrorlist.py
@@ -22,81 +22,97 @@ def trim(input):
     """ remove all but http and ftp URLs,
     and if both http and ftp are offered,
     leave only http"""
-    l = {}
-    for hostid, country, hcurl, siteprivate, hostprivate in input:
+    result = []
+    seen = {}
+    for (directoryname, hostid, country, hcurl, siteprivate, hostprivate) in input:
+        if directoryname not in seen:
+            seen[directoryname] = {}
+    
         us = hcurl.split('/')
         uprotocol = us[0]
         umachine = us[2]
-        if not l.has_key(hostid):
-            l[hostid] = {}
-        l[hostid][uprotocol] = (hostid, country, hcurl, siteprivate, hostprivate)
+        if hostid not in seen[directoryname]:
+            seen[directoryname][hostid] = {}
+        seen[directoryname][hostid][uprotocol] = (directoryname, hostid, country, hcurl, siteprivate, hostprivate)
 
-    result = []
-    for k, v in l.iteritems():
-        if v.has_key(u'http:'):
-            result.append(v[u'http:'])
-        elif v.has_key(u'ftp:'):
-            result.append(v[u'ftp:'])
+    for directoryname in seen.keys():
+        for k, v in seen[directoryname].iteritems():
+            if v.has_key(u'http:'):
+                r = v[u'http:']
+            elif v.has_key(u'ftp:'):
+                r = v[u'ftp:']
+
+            result.append(r)
 
     return result
 
-def _do_query_directory(directory, category):
-    sql  = 'SELECT host.id, host.country, host_category_url.url, site.private, host.private '
-    sql += 'FROM host_category_dir, host_category, host_category_url, host, site '
+
+
+def _do_query_directories():
+    sql  = 'SELECT directory.name, host.id, host.country, host_category_url.url, site.private, host.private '
+    sql += 'FROM directory, host_category_dir, host_category, host_category_url, host, site '
     sql += 'WHERE host_category_dir.host_category_id = host_category.id ' # join criteria
     sql += 'AND   host_category_url.host_category_id = host_category.id ' # join criteria
     sql += 'AND   host_category.host_id = host.id '                       # join criteria
     sql += 'AND   host.site_id = site.id '                                # join criteria
-    sql += 'AND host_category.category_id = %d ' % category.id # but select only the target category
-    sql += "AND host_category_dir.directory_id = %s " % directory.id # and target directory
+    sql += 'AND   host_category_dir.directory_id = directory.id '         # join criteria
     sql += 'AND (host_category_dir.up2date OR host_category.always_up2date) '
     sql += 'AND NOT host_category_url.private '
     sql += 'AND host.user_active AND site.user_active '
     sql += 'AND host.admin_active AND site.admin_active '
+    sql += 'ORDER BY directory.name '
 
+    directory = Directory.select()[0]
     result = directory._connection.queryAll(sql)
-    result = trim(result)
     return result
 
-def populate_directory_cache(directory):
-    repo = directory.repository
-    # if a directory is in more than one category, problem...
-    if repo is not None:
-        category = repo.category
-    else:
-        numcats = len(directory.categories)
-        if numcats == 0:
-            # no category, so we can't know a mirror host's URLs.
-            # nothing to add.
-            return
-        elif numcats >= 1:
-            # any of them will do, so just look at the first one
-            category = directory.categories[0]
-        
-    path = directory.name[len(category.topdir.name)+1:]
-    result = _do_query_directory(directory, category)
-    
-    newresult = {'global':[], 'byCountry':{}, 'byHostId':{}, 'subpath':path}
-    for (hostid, country, hcurl, siteprivate, hostprivate) in result:
+def populate_directory_cache():
+    global repo_arch_to_directoryname
+    result = _do_query_directories()
+    result = trim(result)
+
+    cache = {}
+    for (directoryname, hostid, country, hcurl, siteprivate, hostprivate) in result:
+        if directoryname not in cache:
+            cache[directoryname] = {'global':[], 'byCountry':{}, 'byHostId':{}}
+            directory = Directory.byName(directoryname)
+            repo = directory.repository
+            # if a directory is in more than one category, problem...
+            if repo is not None:
+                repo_arch_to_directoryname[(repo.prefix, repo.arch.name)] = directory.name
+                category = repo.category
+            else:
+                numcats = len(directory.categories)
+                if numcats == 0:
+                    # no category, so we can't know a mirror host's URLs.
+                    # nothing to add.
+                    continue
+                elif numcats >= 1:
+                    # any of them will do, so just look at the first one
+                    category = directory.categories[0]
+                
+            cache[directoryname]['subpath'] = directoryname[len(category.topdir.name)+1:]
+            del repo
+            del directory
+            del category
+            
         country = country.upper()
         v = (hostid, hcurl)
         if not siteprivate and not hostprivate:
-            newresult['global'].append(v)
-            if not newresult['byCountry'].has_key(country):
-                newresult['byCountry'][country] = [v]
-            else:
-                newresult['byCountry'][country].append(v)
+            cache[directoryname]['global'].append(v)
 
-        if not newresult['byHostId'].has_key(hostid):
-            newresult['byHostId'][hostid] = [v]
+            if not cache[directoryname]['byCountry'].has_key(country):
+                cache[directoryname]['byCountry'][country] = [v]
+            else:
+                cache[directoryname]['byCountry'][country].append(v)
+
+        if not cache[directoryname]['byHostId'].has_key(hostid):
+            cache[directoryname]['byHostId'][hostid] = [v]
         else:
-            newresult['byHostId'][hostid].append(v)
+            cache[directoryname]['byHostId'][hostid].append(v)
 
     global mirrorlist_cache
-    mirrorlist_cache[directory.name] = newresult
-    global repo_arch_to_directoryname
-    if repo is not None:
-        repo_arch_to_directoryname[(repo.prefix, repo.arch.name)] = directory.name
+    mirrorlist_cache = cache
 
 def populate_netblock_cache():
     cache = {}
@@ -127,8 +143,7 @@ def populate_host_country_allowed_cache():
 def populate_all_caches():
     populate_host_country_allowed_cache()
     populate_netblock_cache()
-    for d in Directory.select():
-        populate_directory_cache(d)
+    populate_directory_cache()
     print "mirrorlist caches populated"
 
 
