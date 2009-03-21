b45f10cb3bd7859069401b14c2095a665afd5d22
Matt Domsch <matt@domsch.com>
2009-03-20 21:44:53 -0400

    fixup first pass of schema updates script

--- server/mirrormanager/schema_updates/update-1_2_10.py
+++ server/mirrormanager/schema_updates/update-1_2_10.py
@@ -1,5 +1,18 @@
-Arch.addColumn(name='publiclist', updateSchema=True)
-Arch.addColumn(name='primary', updateSchema=True)
+import turbogears
+from sqlobject import *
+from mirrormanager.model import *
 
-FileDetail.addColumn(name='sha256', updateSchema=True)
-FileDetail.addColumn(name='sha512', updateSchema=True)
+def update():
+    Arch.sqlmeta.addColumn(BoolCol('publiclist', default=True), updateSchema=True)
+    Arch.sqlmeta.addColumn(BoolCol('primary', default=True), updateSchema=True)
+    for a in Arch.select():
+        a.publiclist = True
+        a.primary = True
+        a.sync()
+
+    FileDetail.sqlmeta.addColumn(UnicodeCol('sha256', default=None), updateSchema=True)
+    FileDetail.sqlmeta.addColumn(UnicodeCol('sha512', default=None), updateSchema=True)
+    for f in FileDetail.select():
+        f.sha256 = None
+        f.sha512 = None
+        f.sync()
