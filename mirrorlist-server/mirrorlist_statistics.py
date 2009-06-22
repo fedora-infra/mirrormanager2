80ac78a695c0bf893c80f65fe017306a88b42ea4
Adrian Reber <adrian@lisas.de>
2009-06-22 11:44:08 +0200

    mirrorlist-server: re-license mirrorlist_statistics.py under MIT/X11
    instead of previous GPLv3
    
    also a small fix for crashes with broken logfiles

--- mirrorlist-server/mirrorlist_statistics.py
+++ mirrorlist-server/mirrorlist_statistics.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
 #
 # Copyright (C) 2008 by Alexander Koenig
-# Copyright (C) 2008 by Adrian Reber
+# Copyright (C) 2008, 2009 by Adrian Reber
 #
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys, pylab, time, getopt
 
@@ -85,7 +95,10 @@ i = 0
 
 for line in open(logfile):
 	arguments = line.split()
-	y, m, d = arguments[3][:10].split('-')
+	try:
+		y, m, d = arguments[3][:10].split('-')
+	except:
+		continue
         if not ((int(y) == y1) and (int(m) == m1) and (int(d) == d1)):
 		continue
 	try:
