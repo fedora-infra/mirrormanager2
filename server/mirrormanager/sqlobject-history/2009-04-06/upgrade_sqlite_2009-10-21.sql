c12cf12262d8ecece77b19bc4609337a9aad21a6
Matt Domsch <Matt_Domsch@dell.com>
2009-10-21 08:29:07 -0500

    sqlite doesn\'t like setting a BOOL column to TRUE, set it to 1 instead

--- server/mirrormanager/sqlobject-history/2009-04-06/upgrade_sqlite_2009-10-21.sql
+++ server/mirrormanager/sqlobject-history/2009-04-06/upgrade_sqlite_2009-10-21.sql
@@ -1,3 +1,3 @@
 ALTER TABLE host ADD COLUMN asn INT DEFAULT NULL;
 ALTER TABLE host ADD COLUMN asn_clients BOOL DEFAULT TRUE;
-UPDATE host SET asn=NULL, asn_clients=TRUE;
+UPDATE host SET asn=NULL, asn_clients=1;
