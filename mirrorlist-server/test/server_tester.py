4ad121b2d49316c892207bb1c4a4ca8616435956
Matt Domsch <Matt_Domsch@dell.com>
2010-05-11 21:38:11 -0500

    mirrorlist tester: more printing, run forever

--- mirrorlist-server/test/server_tester.py
+++ mirrorlist-server/test/server_tester.py
@@ -4,31 +4,39 @@
 #  by Matt Domsch <Matt_Domsch@dell.com>
 # Licensed under the MIT/X11 license
 
-import socket
+import socket, os
 import cPickle as pickle
 from string import zfill, atoi
+from datetime import datetime
 
-socketfile = '/tmp/mirrormanager_mirrorlist_server.sock'
+socketfile = '/var/run/mirrormanager/mirrorlist_server.sock'
+
+pid = os.getpid()
+connectTime = None
 
 def do_mirrorlist(d):
     try:
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        start = datetime.utcnow()
         s.connect(socketfile)
+        end = datetime.utcnow()
     except:
         raise
 
+    global connectTime
+    connectTime = (end - start)
     p = pickle.dumps(d)
     size = len(p)
-    print "writing size %s to server" % size
+    #print "writing size %s to server" % size
     s.sendall(zfill('%s' % size, 10))
 
     # write the pickle
-    print "writing the pickle"
+    #print "writing the pickle"
     s.sendall(p)
     s.shutdown(socket.SHUT_WR)
     del p
 
-    print "reading result pickle size back"
+    #print "reading result pickle size back"
     readlen = 0
     resultsize = ''
     while readlen < 10:
@@ -36,7 +44,7 @@ def do_mirrorlist(d):
         readlen = len(resultsize)
     resultsize = atoi(resultsize)
     
-    print "reading %s bytes of the results list" % resultsize
+    #print "reading %s bytes of the results list" % resultsize
     readlen = 0
     p = ''
     while readlen < resultsize:
@@ -55,9 +63,12 @@ import random
 
 client_ip = "%s.%s.%s.%s" % (random.randint(0,255), random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
-d = {'repo':'fedora-7',
+d = {'repo':'fedora-12',
      'arch':'i386',
      'client_ip':client_ip}
 
-print do_mirrorlist(d)
-
+while 1:
+  start = datetime.utcnow()
+  result = do_mirrorlist(d)
+  end = datetime.utcnow()
+  print "[%s]   connect: %s  total: %s" % (pid, connectTime, (end-start))
