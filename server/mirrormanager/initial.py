bfc91e3580b7c86504373c4c4188d5824fa7f1b5
Matt Domsch <Matt_Domsch@dell.com>
2013-01-19 08:50:02 -0600

    begin cleanup of initial user creation

--- server/mirrormanager/initial.py
+++ server/mirrormanager/initial.py
@@ -1,15 +1,28 @@
+from sqlobject.dberrors import DuplicateEntryError
 from mirrormanager.model import User, Group
 
 def user_group_setup():
-    Group(group_name='user', display_name='User')
-    Group(group_name='sysadmin', display_name='Admin')
-    a = User(
-        user_name='admin', 
-        email_address='admin@example.com', 
-        display_name='Admin', 
-        password='admin'
-        )
+    try:
+        Group(group_name='user', display_name='User')
+    except DuplicateEntryError:
+        pass
+    try:
+        Group(group_name='sysadmin', display_name='Admin')
+    except DuplicateEntryError:
+        pass
+    try:
+        # fixme cannot create a user and set a password this way
+        # as long as we're using an identity provider.
+        a = User(
+            user_name='admin', 
+            email_address='admin@example.com', 
+            display_name='Admin',
+            password='admin'
+            )
+    except DuplicateEntryError:
+        pass
+    
     a.addGroup(Group.by_group_name('user'))
     a.addGroup(Group.by_group_name('sysadmin'))
-
+        
 user_group_setup()
