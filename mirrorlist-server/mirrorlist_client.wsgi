fedora-infra/mirrormanager2

commit ec0c2db90452367eae2567b1cc1401ad9f2b1a78

mirrorlist_client: don't return redirects to non-HTTP servers
https://fedorahosted.org/fedora-infrastructure/ticket/3610

mirrorlist-server/mirrorlist_client.wsgi
@@ -134,6 +134,13 @@ def manage_compression(request, response, buf):
         response.headers['Content-Encoding'] = 'gzip'
     return buf
 
+def keep_only_http_results(input):
+    output = []
+    for hostid, url in input:
+        if url.startswith(u'http'):
+            output.append((hostid, url))
+    return output
+
 def application(environ, start_response):
     request = WSGIRequest(environ)
     response = WSGIResponse()
@@ -155,12 +162,15 @@ def application(environ, start_response):
         if 'redirect' in request.GET:
             if len(results) == 0:
                 response.status_code=404
-                return response(environ, start_response)
             else: 
-                (hostid, url) = results[0]
-                response.status_code=302
-                response.headers['Location'] = str(url)
-                return response(environ, start_response)
+                results = keep_only_http_results(results)
+                if len(results):
+                    (hostid, url) = results[0]
+                    response.status_code=302
+                    response.headers['Location'] = str(url)
+                else:
+                    response.status_code=404
+        return response(environ, start_response)
 
         text = ""
         text += message + '\n'

commit 84b61ecd465349860ad177722f819bc1b473b2b7

mirrorlist_client: fix mirrorlist/ early return
ec0c2db90452367eae2567b1cc1401ad9f2b1a78
introduced a failure for all mirrorlist/ requests, prematurely
returning.  The root cause was an indentation problem.

mirrorlist-server/mirrorlist_client.wsgi
@@ -170,7 +170,7 @@ def application(environ, start_response):
                     response.headers['Location'] = str(url)
                 else:
                     response.status_code=404
-        return response(environ, start_response)
+            return response(environ, start_response)
 
         text = ""
         text += message + '\n'