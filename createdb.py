aabf295383f0b32d9b7f6a97563072585c173ef0
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-04-23 15:45:19 +0200

    Add the createdb.py script to actually create the database

---
+++ createdb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+## These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+import pkg_resources
+
+from mirrormanager2 import APP
+from mirrormanager2.lib import model
+
+model.create_tables(
+    APP.config['DB_URL'],
+    APP.config.get('PATH_ALEMBIC_INI', None),
+    True)
