f2aa23e321b9276e6cc29a1383c0de5c3fc51a4b
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-04-30 12:34:15 +0200

    pep8 fixes on the python scripts used to create the DB or run the application

--- createdb.py
+++ createdb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-## These two lines are needed to run on EL6
+# These two lines are needed to run on EL6
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 
