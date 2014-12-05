409da181e86629718c459e04ae1f119237ff78fb
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-12-05 19:11:11 +0100

    Add first tests for mirrormanager.lib for test_query_directories
    This was used to check that the query from MM1 and the new query using
    SQLAlchemy were returning the same results

---
+++ tests/test_mmlib.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+'''
+mirrormanager2 tests.
+'''
+
+__requires__ = ['SQLAlchemy >= 0.7']
+import pkg_resources
+
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), '..'))
+
+import mirrormanager2.lib
+from tests import Modeltests
+
+
+class MMLibtests(Modeltests):
+    """ Collection tests. """
+
+    def test_query_directories(self):
+        """ Test the query_directories function of mirrormanager2.lib.
+        """
+        results = mirrormanager2.lib.query_directories(self.session)
+        cnt = 0
+        for row in results:
+            if cnt == 0:
+                print '**', row
+                self.assertEqual(row.directory_id, 3764)
+                self.assertEqual(row.dname, 'pub/alt')
+                self.assertEqual(row.hostid, 271)
+                self.assertEqual(row.country, 'US')
+                self.assertEqual(row.id, 6817)
+            cnt += 1
+        self.assertEqual(cnt, 1693670)
+
+
+if __name__ == '__main__':
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(MMLibtests)
+    unittest.TextTestRunner(verbosity=10).run(SUITE)
