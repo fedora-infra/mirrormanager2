ff3a2304903c85e7141c1248550e00121b2501e4
Ralph Bean <rbean@redhat.com>
2014-12-05 09:01:50 -0500

    Just go ahead and drop those tables.

--- utility/clean_db_for_mm2.sql
+++ utility/clean_db_for_mm2.sql
@@ -14,20 +14,21 @@ DROP TABLE "tg_user";
 DROP TABLE "visit";
 
 
--- If you fear there is some data in these table this will rename and
--- adjust them
-
-ALTER TABLE "user_group" RENAME TO "mm_user_group";
-ALTER TABLE "tg_group" RENAME TO "mm_group";
-
-ALTER TABLE "tg_user" RENAME TO "mm_user";
-ALTER TABLE "mm_user" ADD COLUMN token varchar(50);
-ALTER TABLE "mm_user" ADD COLUMN updated_on timestamp without time zone;
-
-ALTER TABLE "visit" RENAME TO "mm_user_visit";
-ALTER TABLE "mm_user_visit" ADD COLUMN user_id integer;
-ALTER TABLE ONLY "mm_user_visit"
-    ADD CONSTRAINT user_id_exists FOREIGN KEY (user_id) REFERENCES mm_user(id);
+-- -- If you fear there is some data in these table this will rename and
+-- -- adjust them.  However, in development at the MM2 FAD we have just been
+-- deleting and recreating them.. so, commenting this out.
+-- 
+-- ALTER TABLE "user_group" RENAME TO "mm_user_group";
+-- ALTER TABLE "tg_group" RENAME TO "mm_group";
+-- 
+-- ALTER TABLE "tg_user" RENAME TO "mm_user";
+-- ALTER TABLE "mm_user" ADD COLUMN token varchar(50);
+-- ALTER TABLE "mm_user" ADD COLUMN updated_on timestamp without time zone;
+-- 
+-- ALTER TABLE "visit" RENAME TO "mm_user_visit";
+-- ALTER TABLE "mm_user_visit" ADD COLUMN user_id integer;
+-- ALTER TABLE ONLY "mm_user_visit"
+--     ADD CONSTRAINT user_id_exists FOREIGN KEY (user_id) REFERENCES mm_user(id);
 
 
 -- Add some indexes
