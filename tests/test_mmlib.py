0b4e77dae4b9b5813ee783ce5790a7eac6e90949
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-07 11:54:53 +0100

    Adjust the tests for mirrormanager2.lib.get_repo_by_name

--- tests/test_mmlib.py
+++ tests/test_mmlib.py
@@ -629,6 +629,33 @@ class MMLibtests(tests.Modeltests):
             self.session, 'updates-testing-f20', 'i386')
         self.assertEqual(results, None)
 
+    def test_get_repo_by_name(self):
+        """ Test the get_repo_by_name function of mirrormanager2.lib.
+        """
+        results = mirrormanager2.lib.get_repo_by_name(
+            self.session, 'pub/fedora/linux/updates/testing/19/x86_64')
+        self.assertEqual(results, None)
+
+        tests.create_base_items(self.session)
+        tests.create_version(self.session)
+        tests.create_directory(self.session)
+        tests.create_category(self.session)
+        tests.create_repository(self.session)
+
+        results = mirrormanager2.lib.get_repo_by_name(
+            self.session, 'pub/fedora/linux/updates/testing/19/x86_64')
+        self.assertEqual(
+            results.name, 'pub/fedora/linux/updates/testing/19/x86_64')
+
+        results = mirrormanager2.lib.get_repo_by_name(
+            self.session, 'pub/fedora/linux/updates/testing/20/x86_64')
+        self.assertEqual(
+            results.name, 'pub/fedora/linux/updates/testing/20/x86_64')
+
+        results = mirrormanager2.lib.get_repo_by_name(
+            self.session, 'pub/fedora/linux/updates/testing/19/i386')
+        self.assertEqual(results, None)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(MMLibtests)
