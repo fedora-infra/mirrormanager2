b291ab8088df1e1cf192eab2bb001e36849cafbc
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-08 12:52:58 +0100

    Add unit-test for the hoststatsview in the admin controller

--- tests/test_ui_admin.py
+++ tests/test_ui_admin.py
@@ -497,6 +497,28 @@ class FlaskUiAdminTest(tests.Modeltests):
             self.assertTrue(
                 '<a href="javascript:void(0)">List (0)</a>' in output.data)
 
+    @patch('mirrormanager2.app.is_mirrormanager_admin')
+    def test_admin_hoststatsview(self, login_func):
+        """ Test the admin Host Stats view. """
+        login_func.return_value = None
+
+        user = tests.FakeFasUserAdmin()
+        with tests.user_set(mirrormanager2.app.APP, user):
+            output = self.app.get('/admin/hoststatsview/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Host - Host Stats - Admin'
+                '</title>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/archview/">Arch</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/hoststatsview/?sort=2" '
+                'title="Sort by Type">Type</a>' in output.data)
+            self.assertTrue(
+                '<a href="javascript:void(0)">List (0)</a>' in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskUiAdminTest)
