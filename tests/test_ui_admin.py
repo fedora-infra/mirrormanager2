8329f6b5629016fc7f2e50ecff7dff36b0799d0d
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-08 12:58:37 +0100

    Add unit-test for the productview in the admin controller

--- tests/test_ui_admin.py
+++ tests/test_ui_admin.py
@@ -565,6 +565,28 @@ class FlaskUiAdminTest(tests.Modeltests):
             self.assertTrue(
                 '<a href="javascript:void(0)">List (1)</a>' in output.data)
 
+    @patch('mirrormanager2.app.is_mirrormanager_admin')
+    def test_admin_productview(self, login_func):
+        """ Test the admin Product view. """
+        login_func.return_value = None
+
+        user = tests.FakeFasUserAdmin()
+        with tests.user_set(mirrormanager2.app.APP, user):
+            output = self.app.get('/admin/productview/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Product - Admin'
+                '</title>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/archview/">Arch</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/productview/?sort=0" '
+                'title="Sort by Name">Name</a>' in output.data)
+            self.assertTrue(
+                '<a href="javascript:void(0)">List (2)</a>' in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskUiAdminTest)
