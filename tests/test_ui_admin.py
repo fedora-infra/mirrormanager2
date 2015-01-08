b3bb9b4de5a2c0ca11856f4d06074e077adc9bdc
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-08 12:24:53 +0100

    Add unit-test for the countrycontinentredirectview in the admin controller

--- tests/test_ui_admin.py
+++ tests/test_ui_admin.py
@@ -132,6 +132,30 @@ class FlaskUiAdminTest(tests.Modeltests):
             self.assertTrue(
                 '<a href="javascript:void(0)">List (2)</a>' in output.data)
 
+    @patch('mirrormanager2.app.is_mirrormanager_admin')
+    def test_admin_countrycontinentredirectview(self, login_func):
+        """ Test the admin CountryContinentRedirect view. """
+        login_func.return_value = None
+
+        user = tests.FakeFasUserAdmin()
+        with tests.user_set(mirrormanager2.app.APP, user):
+            output = self.app.get('/admin/countrycontinentredirectview/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Country - Country Continent Redirect - Admin'
+                '</title>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/archview/">Arch</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/countrycontinentredirectview/?sort=0" '
+                'title="Sort by Country">Country</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertTrue(
+                '<a href="javascript:void(0)">List (3)</a>' in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskUiAdminTest)
