3704f9dc2fcfe6b918341ffb15cdd12593e0e6e0
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-08 12:45:48 +0100

    Add unit-test for the hostlocationview in the admin controller

--- tests/test_ui_admin.py
+++ tests/test_ui_admin.py
@@ -432,6 +432,27 @@ class FlaskUiAdminTest(tests.Modeltests):
             self.assertTrue(
                 '<a href="javascript:void(0)">List (0)</a>' in output.data)
 
+    @patch('mirrormanager2.app.is_mirrormanager_admin')
+    def test_admin_hostlocationview(self, login_func):
+        """ Test the admin Host Location view. """
+        login_func.return_value = None
+
+        user = tests.FakeFasUserAdmin()
+        with tests.user_set(mirrormanager2.app.APP, user):
+            output = self.app.get('/admin/hostlocationview/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Host - Host Location - Admin'
+                '</title>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/archview/">Arch</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertEqual(
+                output.data.count('<th class="column-header">'), 2)
+            self.assertTrue(
+                '<a href="javascript:void(0)">List (0)</a>' in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskUiAdminTest)
