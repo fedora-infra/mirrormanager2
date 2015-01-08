ac227eb29bed3803b6d616c625c7c6f321e16e07
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-08 12:44:24 +0100

    Add unit-test for the hostcountryallowedview in the admin controller

--- tests/test_ui_admin.py
+++ tests/test_ui_admin.py
@@ -410,6 +410,28 @@ class FlaskUiAdminTest(tests.Modeltests):
             self.assertTrue(
                 '<a href="javascript:void(0)">List (0)</a>' in output.data)
 
+    @patch('mirrormanager2.app.is_mirrormanager_admin')
+    def test_admin_hostcountryallowedview(self, login_func):
+        """ Test the admin Host Country Allowed view. """
+        login_func.return_value = None
+
+        user = tests.FakeFasUserAdmin()
+        with tests.user_set(mirrormanager2.app.APP, user):
+            output = self.app.get('/admin/hostcountryallowedview/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Host - Host Country Allowed - Admin'
+                '</title>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/archview/">Arch</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/hostcountryallowedview/?sort=1" '
+                'title="Sort by Country">Country</a>' in output.data)
+            self.assertTrue(
+                '<a href="javascript:void(0)">List (0)</a>' in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskUiAdminTest)
