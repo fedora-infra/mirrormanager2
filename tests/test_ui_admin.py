fc19bc7256be95005d7e4fc1428d7e50ad8c2527
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-08 12:38:55 +0100

    Add unit-test for the hostcategoryview in the admin controller

--- tests/test_ui_admin.py
+++ tests/test_ui_admin.py
@@ -322,6 +322,28 @@ class FlaskUiAdminTest(tests.Modeltests):
             self.assertTrue(
                 '<a href="javascript:void(0)">List (0)</a>' in output.data)
 
+    @patch('mirrormanager2.app.is_mirrormanager_admin')
+    def test_admin_hostcategoryview(self, login_func):
+        """ Test the admin Host Category view. """
+        login_func.return_value = None
+
+        user = tests.FakeFasUserAdmin()
+        with tests.user_set(mirrormanager2.app.APP, user):
+            output = self.app.get('/admin/hostcategoryview/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Host - Host Category - Admin'
+                '</title>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/archview/">Arch</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/hostcategoryview/?sort=2" '
+                'title="Sort by Always Up2Date">Always Up2Date</a>' in output.data)
+            self.assertTrue(
+                '<a href="javascript:void(0)">List (4)</a>' in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskUiAdminTest)
