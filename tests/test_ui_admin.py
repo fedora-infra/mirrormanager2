76e531a135e13ed497e69af0b7e0a6afcab0f8f7
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-08 13:03:40 +0100

    Add unit-test for the siteview in the admin controller

--- tests/test_ui_admin.py
+++ tests/test_ui_admin.py
@@ -634,6 +634,28 @@ class FlaskUiAdminTest(tests.Modeltests):
             self.assertTrue(
                 '<a href="javascript:void(0)">List (3)</a>' in output.data)
 
+    @patch('mirrormanager2.app.is_mirrormanager_admin')
+    def test_admin_siteview(self, login_func):
+        """ Test the admin Site view. """
+        login_func.return_value = None
+
+        user = tests.FakeFasUserAdmin()
+        with tests.user_set(mirrormanager2.app.APP, user):
+            output = self.app.get('/admin/siteview/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Site - Site - Admin'
+                '</title>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/archview/">Arch</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/siteview/?sort=0" '
+                'title="Sort by Name">Name</a>' in output.data)
+            self.assertTrue(
+                '<a href="javascript:void(0)">List (3)</a>' in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskUiAdminTest)
