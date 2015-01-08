d9e6e6af2ac8b6813a9f4d6e5528f0603caea411
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-08 12:57:38 +0100

    Add unit-test for the netblockcountryview in the admin controller

--- tests/test_ui_admin.py
+++ tests/test_ui_admin.py
@@ -46,6 +46,7 @@ class FlaskUiAdminTest(tests.Modeltests):
         tests.create_site(self.session)
         tests.create_hosts(self.session)
         tests.create_location(self.session)
+        tests.create_netblockcountry(self.session)
         tests.create_directory(self.session)
         tests.create_category(self.session)
         tests.create_hostcategory(self.session)
@@ -541,6 +542,28 @@ class FlaskUiAdminTest(tests.Modeltests):
                 'title="Sort by Name">Name</a>' in output.data)
             self.assertTrue(
                 '<a href="javascript:void(0)">List (3)</a>' in output.data)
+
+    @patch('mirrormanager2.app.is_mirrormanager_admin')
+    def test_admin_netblockcountryview(self, login_func):
+        """ Test the admin Netblock Country view. """
+        login_func.return_value = None
+
+        user = tests.FakeFasUserAdmin()
+        with tests.user_set(mirrormanager2.app.APP, user):
+            output = self.app.get('/admin/netblockcountryview/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Netblock Country - Admin'
+                '</title>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/archview/">Arch</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/netblockcountryview/?sort=0" '
+                'title="Sort by Netblock">Netblock</a>' in output.data)
+            self.assertTrue(
+                '<a href="javascript:void(0)">List (1)</a>' in output.data)
 
 
 if __name__ == '__main__':
