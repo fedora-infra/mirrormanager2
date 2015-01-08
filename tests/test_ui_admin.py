05a8f5dfcaf8bd94811052fa83b10ca6a6103fd9
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-08 12:33:10 +0100

    Add unit-test for the filedetailview in the admin controller

--- tests/test_ui_admin.py
+++ tests/test_ui_admin.py
@@ -213,6 +213,28 @@ class FlaskUiAdminTest(tests.Modeltests):
             self.assertTrue(
                 '<a href="javascript:void(0)">List (0)</a>' in output.data)
 
+    @patch('mirrormanager2.app.is_mirrormanager_admin')
+    def test_admin_filedetailview(self, login_func):
+        """ Test the admin FileDetail view. """
+        login_func.return_value = None
+
+        user = tests.FakeFasUserAdmin()
+        with tests.user_set(mirrormanager2.app.APP, user):
+            output = self.app.get('/admin/filedetailview/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>File - File Detail - Admin'
+                '</title>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/archview/">Arch</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/categoryview/">Category</a>' in output.data)
+            self.assertTrue(
+                '<a href="/admin/filedetailview/?sort=1" '
+                'title="Sort by Filename">Filename</a>' in output.data)
+            self.assertTrue(
+                '<a href="javascript:void(0)">List (0)</a>' in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskUiAdminTest)
