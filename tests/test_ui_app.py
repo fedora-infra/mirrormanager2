53178f0599e90f732b4b1a27aa1acb656fa627f6
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-01-09 10:25:47 +0100

    Add unit-tests for the host_netblock_delete method

--- tests/test_ui_app.py
+++ tests/test_ui_app.py
@@ -832,6 +832,84 @@ class FlaskUiAppTest(tests.Modeltests):
             self.assertTrue(
                 'action="/host/3/host_netblock/1/delete">' in output.data)
 
+    def test_host_netblock_delete(self):
+        """ Test the host_netblock_delete endpoint. """
+        # Create an Host Netblock to delete
+        self.test_host_netblock_new()
+
+        output = self.app.post('/host/3/host_netblock/1/delete')
+        self.assertEqual(output.status_code, 302)
+        output = self.app.post(
+            '/host/3/host_netblock/1/delete', follow_redirects=True)
+        self.assertEqual(output.status_code, 200)
+        self.assertTrue(
+            '<title>OpenID transaction in progress</title>' in output.data)
+
+        user = tests.FakeFasUser()
+        with tests.user_set(mirrormanager2.app.APP, user):
+
+            # Check before adding the host
+            output = self.app.get('/host/3')
+            self.assertEqual(output.status_code, 200)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<h2>Host private.localhost</h2>' in output.data)
+            self.assertTrue('Back to <a href="/site/1">' in output.data)
+            self.assertTrue(
+                '<title>Host - MirrorManager</title>' in output.data)
+            self.assertTrue(
+                'action="/host/3/host_netblock/1/delete">' in output.data)
+
+            csrf_token = output.data.split(
+                'name="csrf_token" type="hidden" value="')[1].split('">')[0]
+
+            data = {}
+
+            # Check CSRF protection
+
+            output = self.app.post(
+                '/host/3/host_netblock/1/delete', data=data,
+                follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<h2>Host private.localhost</h2>' in output.data)
+            self.assertTrue('Back to <a href="/site/1">' in output.data)
+            self.assertTrue(
+                '<title>Host - MirrorManager</title>' in output.data)
+            self.assertTrue(
+                'action="/host/3/host_netblock/1/delete">' in output.data)
+
+            # Delete Site Admin
+
+            data['csrf_token'] = csrf_token
+
+            # Invalid site identifier
+            output = self.app.post(
+                '/host/5/host_netblock/1/delete', data=data,
+                follow_redirects=True)
+            self.assertEqual(output.status_code, 404)
+            self.assertTrue('<p>Host not found</p>' in output.data)
+
+            # Invalid site admin identifier
+            output = self.app.post(
+                '/host/3/host_netblock/2/delete', data=data,
+                follow_redirects=True)
+            self.assertEqual(output.status_code, 404)
+            self.assertTrue('<p>Host netblock not found</p>' in output.data)
+
+            # Delete Site Admin
+            output = self.app.post(
+                '/host/3/host_netblock/1/delete', data=data,
+                follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<li class="message">Host netblock deleted</li>'
+                in output.data)
+            self.assertTrue('<h2>Host private.localhost</h2>' in output.data)
+            self.assertTrue('Back to <a href="/site/1">' in output.data)
+            self.assertTrue(
+                '<title>Host - MirrorManager</title>' in output.data)
+            self.assertFalse(
+                'action="/host/3/host_netblock/1/delete">' in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FlaskUiAppTest)
