571d1d42d4daaa027a342e6d681605dfd0bca6b8
David Gay <dgay@redhat.com>
2015-03-29 19:16:09 -0700

    add a file header

--- utility/validate_pickle.py
+++ utility/validate_pickle.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+#
+# This file is part of MirrorManager 2.
+#
+# validate_pickle.py by David Gay <dgay@redhat.com>
+# Runs a series of checks on a MM2 pickle file to confirm its validity.
+#
+# Released under the MIT/X11 license.
 
 import cPickle
 import sys
