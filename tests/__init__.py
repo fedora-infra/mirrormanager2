15ce4cfc6368ff040e6fe0de1f1e51a47f01acd0
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-08-04 15:27:16 +0200

    Adjust the faitout url to the new address

--- tests/__init__.py
+++ tests/__init__.py
@@ -25,7 +25,7 @@ from mirrormanager2.lib import model
 #DB_PATH = 'sqlite:///:memory:'
 ## A file database is required to check the integrity, don't ask
 DB_PATH = 'sqlite:////tmp/test.sqlite'
-FAITOUT_URL = 'http://209.132.184.152/faitout/'
+FAITOUT_URL = 'http://faitout.fedorainfracloud.org/'
 
 if os.environ.get('BUILD_ID'):
     try:
