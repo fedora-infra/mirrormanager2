644b1758beb3c06f5a3a5b85f2542864af4eef82
Adrian Reber <adrian@lisas.de>
2015-11-13 10:40:24 +0100

    Fix license text
    The file mirrorlist_statistics.py was re-licensed in commit
    80ac78a695c0bf893c80f65fe017306a88b42ea4. Unfortunately the GPLv3
    license text was still partly included. This is now removed.
    
    Signed-off-by: Adrian Reber <adrian@lisas.de>

--- mirrorlist/mirrorlist_statistics.py
+++ mirrorlist/mirrorlist_statistics.py
@@ -21,10 +21,6 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
 
 import sys, pylab, time, getopt
 
