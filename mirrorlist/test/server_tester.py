18e71bb985e1dc30d52f007b3b14a16a64579d86
Adrian Reber <adrian@lisas.de>
2019-03-22 15:35:25 +0100

    mirrorlist: make the server_tester work with Python 2 and 3
    Signed-off-by: Adrian Reber <adrian@lisas.de>

--- mirrorlist/test/server_tester.py
+++ mirrorlist/test/server_tester.py
@@ -4,7 +4,6 @@
 #  by Matt Domsch <Matt_Domsch@dell.com>
 # Licensed under the MIT/X11 license
 
-from __future__ import print_function
 import socket, os
 try:
     import cPickle as pickle
@@ -31,26 +30,26 @@ def do_mirrorlist(d):
     connectTime = (end - start)
     p = pickle.dumps(d)
     size = len(p)
-    #print "writing size %s to server" % size
-    s.sendall(str(size).zfill(10))
+    #print("writing size %s to server" % size)
+    s.sendall(str(size).encode().zfill(10))
 
     # write the pickle
-    #print "writing the pickle"
+    #print("writing the pickle")
     s.sendall(p)
     s.shutdown(socket.SHUT_WR)
     del p
 
-    #print "reading result pickle size back"
+    #print("reading result pickle size back")
     readlen = 0
-    resultsize = ''
+    resultsize = b''
     while readlen < 10:
         resultsize += s.recv(10 - readlen)
         readlen = len(resultsize)
     resultsize = int(resultsize)
 
-    #print "reading %s bytes of the results list" % resultsize
+    #print("reading %s bytes of the results list" % resultsize)
     readlen = 0
-    p = ''
+    p = b''
     while readlen < resultsize:
         p += s.recv(resultsize - readlen)
         readlen = len(p)
@@ -68,17 +67,17 @@ import random
 while True:
     client_ip = "%s.%s.%s.%s" % (random.randint(0,255), random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
-    d = {'repo':'fedora-18',
-         'arch':'i386',
+    d = {'repo':'fedora-28',
+         'arch':'x86_64',
          'metalink':False}
 
-    for k, v in d.items():
+    for k, v in list(d.items()):
         try:
-            d[k] = unicode(v, 'utf8', 'replace')
+            d[k] = str(v, 'utf8', 'replace')
         except:
             pass
 
-    client_ip = u"%s.%s.%s.%s" % (random.randint(0,255), random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    client_ip = "%s.%s.%s.%s" % (random.randint(0,255), random.randint(0,255), random.randint(0,255), random.randint(0,255))
     d['client_ip'] = client_ip
 
     start = datetime.datetime.utcnow()
