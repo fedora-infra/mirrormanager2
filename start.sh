aceacfda12d8a6cc339a90f438fa1d25d9eb887f
zengchen1024 <chenzeng2@huawei.com>
2023-04-28 18:42:23 +0800

    fix start script

--- start.sh
+++ start.sh
@@ -6,35 +6,35 @@ port=$1
 
 f=mirrormanager2/client_secrets.json
 
-sed -i "s/{OIDC_CLIENT_ID}/$OIDC_CLIENT_ID/"  $f
-sed -i "s/{OIDC_CLIENT_SECRET}/$OIDC_CLIENT_SECRET/"  $f
-sed -i "s/{OIDC_AUTH_URI}/$OIDC_AUTH_URI/"  $f
-sed -i "s/{OIDC_TOKEN_URI}/$OIDC_TOKEN_URI/"  $f
-sed -i "s/{MM_ENDPOINT}/$MM_ENDPOINT/"  $f
-sed -i "s/{OIDC_USERINFO_URI}/$OIDC_USERINFO_URI/"  $f
-sed -i "s/{OIDC_ISSUER}/$OIDC_ISSUER/"  $f
+sed -i "s|{OIDC_CLIENT_ID}|$OIDC_CLIENT_ID|"  $f
+sed -i "s|{OIDC_CLIENT_SECRET}|$OIDC_CLIENT_SECRET|"  $f
+sed -i "s|{OIDC_AUTH_URI}|$OIDC_AUTH_URI|"  $f
+sed -i "s|{OIDC_TOKEN_URI}|$OIDC_TOKEN_URI|"  $f
+sed -i "s|{MM_ENDPOINT}|$MM_ENDPOINT|"  $f
+sed -i "s|{OIDC_USERINFO_URI}|$OIDC_USERINFO_URI|"  $f
+sed -i "s|{OIDC_ISSUER}|$OIDC_ISSUER|"  $f
 
 ###
 
 f=mirrormanager2/default_config.py
 
-sed -i "s/{DB_URL}/$DB_URL/"  $f
-sed -i "s/{SECRET_KEY}/$SECRET_KEY/"  $f
-sed -i "s/{PASSWORD_SEED}/$PASSWORD_SEED/"  $f
+sed -i "s|{DB_URL}|$DB_URL|"  $f
+sed -i "s|{SECRET_KEY}|$SECRET_KEY|"  $f
+sed -i "s|{PASSWORD_SEED}|$PASSWORD_SEED|"  $f
 
 ADMIN_GROUP="\'$ADMIN_GROUP\'"
-ADMIN_GROUP=$(echo $ADMIN_GROUP | sed -e "s/,/','/g")
+ADMIN_GROUP=$(echo $ADMIN_GROUP | sed -e "s|,|','|g")
 
-sed -i "s/{ADMIN_GROUP}/$ADMIN_GROUP/"  $f
+sed -i "s|{ADMIN_GROUP}|$ADMIN_GROUP|"  $f
 
 ###
 
 f=utility/config/mirrormanager2.cfg
 
-sed -i "s/{DB_URL}/$DB_URL/"  $f
-sed -i "s/{SECRET_KEY}/$SECRET_KEY/"  $f
-sed -i "s/{PASSWORD_SEED}/$PASSWORD_SEED/"  $f
-sed -i "s/{UMDL_PREFIX}/$UMDL_PREFIX/"  $f
+sed -i "s|{DB_URL}|$DB_URL|"  $f
+sed -i "s|{SECRET_KEY}|$SECRET_KEY|"  $f
+sed -i "s|{PASSWORD_SEED}|$PASSWORD_SEED|"  $f
+sed -i "s|{UMDL_PREFIX}|$UMDL_PREFIX|"  $f
 
 # step 2. create db
 
