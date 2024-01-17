ab98f4c07ec0357b62ff0a6e9a5c66685dfd8603
Aurélien Bompard <aurelien@bompard.org>
2024-01-17 17:18:18 +0100

    Make utility scripts load the default config
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- mirrormanager2/utility/common.py
+++ mirrormanager2/utility/common.py
@@ -1,5 +1,11 @@
+from mirrormanager2 import default_config
+
+
 def read_config(filename):
     config = dict()
+    for key in dir(default_config):
+        if key.isupper():
+            config[key] = getattr(default_config, key)
     with open(filename) as fh:
         exec(compile(fh.read(), filename, "exec"), config)
     return config
