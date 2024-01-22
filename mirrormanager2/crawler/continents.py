f6e5b8d4c553704898a120703f04fbbc851e649f
Aurélien Bompard <aurelien@bompard.org>
2024-01-22 13:37:59 +0100

    WIP: crawler refactoring
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ mirrormanager2/crawler/continents.py
@@ -0,0 +1,102 @@
+import csv
+import functools
+import logging
+import os
+import socket
+from functools import cache
+from importlib import resources
+from urllib.parse import urlparse
+
+import geoip2
+
+import mirrormanager2.lib
+
+from .constants import CONTINENTS
+
+logger = logging.getLogger("crawler")
+
+
+class WrongContinent(Exception):
+    pass
+
+
+class EmbargoedCountry(Exception):
+    def __init__(self, country):
+        self.country = country
+
+
+class BrokenBaseUrl(ValueError):
+    pass
+
+
+def filter_continents(asked):
+    continents = []
+    for continent in CONTINENTS:
+        if f"^{continent}" in asked:
+            continue
+        if asked and continent not in asked:
+            continue
+        continents.append(continent)
+    return continents
+
+
+@functools.cache
+def get_country_continents(session):
+    country_continent_csv = resources.files("mirrormanager2.crawler").joinpath(
+        "country_continent.csv"
+    )
+    new_country_continents = {}
+    with country_continent_csv.open("r") as infile:
+        reader = csv.reader(infile)
+        new_country_continents = {rows[0]: rows[1] for rows in reader}
+    for c in mirrormanager2.lib.get_country_continent_redirect(session):
+        new_country_continents[c.country] = c.continent
+    return new_country_continents
+
+
+@cache
+def get_geoip(base_dir):
+    return geoip2.database.Reader(os.path.join(base_dir, "GeoLite2-Country.mmdb"))
+
+
+def check_continent(config, options, session, categoryUrl):
+    gi = get_geoip(config["GEOIP_BASE"])
+    continents = filter_continents(options["continents"])
+    country_continents = get_country_continents(session)
+    # Before the first network access to the mirror let's
+    # check if continent mode is enabled and verfiy if
+    # the mirror is on the target continent.
+    # The continent check takes the first URL of the first category
+    # for the decision on which continent the mirror is.
+    try:
+        hostname = urlparse.urlsplit(categoryUrl)[1]
+    except Exception as e:
+        # Not being able the split the URL is strange.
+        # Something is broken.
+        raise BrokenBaseUrl() from e
+
+    # The function urlsplit() does not remove ':' in case someone
+    # specified a port. Only look at the first element before ':'
+    hostname = hostname.split(":")[0]
+
+    try:
+        hostname = socket.gethostbyname(hostname)
+    except Exception as e:
+        # Name resolution failed. This means
+        # that the base URL is broken.
+        raise BrokenBaseUrl() from e
+
+    country = gi.country(hostname).country.iso_code
+    if not country:
+        # For hosts with no country in the GeoIP database
+        # the default is 'US' as that is where most of
+        # Fedora infrastructure systems are running
+        country = "US"
+    if country in config["EMBARGOED_COUNTRIES"]:
+        raise EmbargoedCountry(country)
+    if country_continents[country] in continents:
+        return
+    # And another return value. '8' is used for mirrors on
+    # the wrong continent. The crawl should not be listed in
+    # the database at all.
+    raise WrongContinent
---
+++ mirrormanager2/crawler/http_connector.py
@@ -0,0 +1,105 @@
+import logging
+from contextlib import suppress
+
+import requests
+
+from .connector import Connector, FetchingFailed, ForbiddenExpected
+from .constants import HTTP_TIMEOUT
+
+logger = logging.getLogger("crawler")
+
+
+class HTTPConnector(Connector):
+    def _connect(self, netloc):
+        session = requests.Session()
+        session.headers = {
+            "Connection": "Keep-Alive",
+            "Pragma": "no-cache",
+            "User-Agent": "mirrormanager-crawler/0.1 (+https://"
+            "github.com/fedora-infra/mirrormanager2/)",
+        }
+        return session
+
+    def _close(self):
+        self._connection.close()
+
+    def check_url(self, url):
+        conn = self.open(url)
+        response = conn.head(url, timeout=HTTP_TIMEOUT)
+        return response.ok
+
+    def _get_content_length(self, conn, url, readable, recursion=0, retry=0):
+        response = conn.head(url, timeout=HTTP_TIMEOUT)
+        if response.ok:
+            try:
+                return response.headers["Content-Length"]
+            except KeyError:
+                return None
+        if response.status_code == 404 or response.status_code == 410:
+            # Not Found / Gone
+            return False
+        if response.status_code == 403:
+            # may be a hidden dir still
+            if readable:
+                return False
+            else:
+                raise ForbiddenExpected()
+        response.raise_for_status()
+
+    def _check_file(self, conn, url, filedata, readable):
+        """Returns tuple:
+        True - URL exists
+        False - URL doesn't exist
+        None - we don't know
+        """
+        try:
+            content_length = self._get_content_length(conn, url, readable)
+        except Exception:
+            return False
+        # lighttpd returns a Content-Length for directories
+        # apache and nginx do not
+        # For the basic check in check_for_base_dir() it is only
+        # relevant if the directory exists or not. Therefore
+        # passing None as filedata[]. This needs to be handled here.
+        if filedata is None:
+            # The file/directory seems to exist
+            return True
+        # fixme should check last_modified too
+        if content_length not in (None, False) and float(filedata["size"]) != float(content_length):
+            return False
+
+        # handle no content-length header, streaming/chunked return
+        # or zero-length file
+        return True
+
+    def _check_dir(self, url, directory):
+        try:
+            conn = self.open(url)
+        except Exception as e:
+            logger.info(f"Could not get {url}: {e}")
+            return None
+        for filename in directory.files:
+            file_url = f"{url}/{filename}"
+            exists = self._check_file(conn, file_url, directory.files[filename], directory.readable)
+            if filename == "repomd.xml" and exists:
+                # Additional optional check
+                with suppress(Exception):
+                    exists = self.compare_sha256(directory, filename, file_url)
+            if exists in (False, None):
+                # Shortcut: we don't need to go over other files
+                return exists
+        return True
+
+    def _get_file(self, url):
+        conn = self.open(url)
+        r = conn.get(
+            url,
+            timeout=HTTP_TIMEOUT,
+        )
+        if not r.ok:
+            raise FetchingFailed(r)
+        return r.content
+
+
+class HTTPSConnector(HTTPConnector):
+    pass
