9ab3908b916f32633660ca891ed6b8e635179800
Aurélien Bompard <aurelien@bompard.org>
2024-02-27 09:32:41 +0100

    Put the default timeout values in the constants module
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- mirrormanager2/crawler/constants.py
+++ mirrormanager2/crawler/constants.py
@@ -5,6 +5,9 @@ CONTINENTS = ["AF", "AN", "AS", "EU", "NA", "OC", "SA", "--"]
 REPODATA_DIR = "repodata"
 REPODATA_FILE = "repomd.xml"
 
+DEFAULT_GLOBAL_TIMEOUT = 360  # minutes
+DEFAULT_HOST_TIMEOUT = 240  # minutes
+
 # number of minutes to wait if a signal is received to shutdown the crawler
 SHUTDOWN_TIMEOUT = 5
 
