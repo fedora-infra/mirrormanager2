19cf885afe6f36b1ba5be94029f098d8e627f9b4
Aurélien Bompard <aurelien@bompard.org>
2024-03-06 07:57:28 +0100

    Run mark_not_up2date after the crawls to avoid a deadlock
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- mirrormanager2/lib/database.py
+++ mirrormanager2/lib/database.py
@@ -1,36 +1,36 @@
-import sqlalchemy as sa
+# import sqlalchemy as sa
 from sqlalchemy_helpers import DatabaseManager, get_base
 
+# class MirrorManagerBaseMixin:
+#     """Base mixin for mirrormanager2 models.
+#
+#     This base class mixin grants sqlalchemy models dict-like access so that
+#     they behave somewhat similarly to SQLObject models (inherited from the TG1
+#     codebase of mirrormanager1).  This was added with the intent to make the
+#     porting of backend scripts from mirrormanager1 to mirrormanager2 easier.
+#     """
+#
+#     def __getitem__(self, key):
+#         return getattr(self, key)
+#
+#     def __setitem__(self, key, value):
+#         setattr(self, key, value)
+#
+#     def __contains__(self, key):
+#         return hasattr(self, key)
+#
+#     @classmethod
+#     def get(cls, session, pkey_value):
+#         primary_keys = [key.key for key in cls.__mapper__.primary_key]
+#         return (
+#             session.query(cls)
+#             .filter(sa.or_(getattr(cls, col) == pkey_value for col in primary_keys))
+#             .one()
+#         )
 
-class MirrorManagerBaseMixin:
-    """Base mixin for mirrormanager2 models.
 
-    This base class mixin grants sqlalchemy models dict-like access so that
-    they behave somewhat similarly to SQLObject models (inherited from the TG1
-    codebase of mirrormanager1).  This was added with the intent to make the
-    porting of backend scripts from mirrormanager1 to mirrormanager2 easier.
-    """
-
-    def __getitem__(self, key):
-        return getattr(self, key)
-
-    def __setitem__(self, key, value):
-        setattr(self, key, value)
-
-    def __contains__(self, key):
-        return hasattr(self, key)
-
-    @classmethod
-    def get(cls, session, pkey_value):
-        primary_keys = [key.key for key in cls.__mapper__.primary_key]
-        return (
-            session.query(cls)
-            .filter(sa.or_(getattr(cls, col) == pkey_value for col in primary_keys))
-            .one()
-        )
-
-
-BASE = get_base(cls=MirrorManagerBaseMixin)
+# BASE = get_base(cls=MirrorManagerBaseMixin)
+BASE = get_base()
 
 
 def get_db_manager(config, **engine_args):
