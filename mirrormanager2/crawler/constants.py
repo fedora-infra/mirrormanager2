919ae0b1ed28b90c23d581bc2ed23a28209f1ff3
Aurélien Bompard <aurelien@bompard.org>
2024-03-06 19:25:58 +0100

    Better threadpool error reporting
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- mirrormanager2/crawler/constants.py
+++ mirrormanager2/crawler/constants.py
@@ -6,7 +6,7 @@ REPODATA_DIR = "repodata"
 REPODATA_FILE = "repomd.xml"
 
 DEFAULT_GLOBAL_TIMEOUT = 360  # minutes
-DEFAULT_HOST_TIMEOUT = 240  # minutes
+DEFAULT_HOST_TIMEOUT = 190  # minutes
 
 # number of minutes to wait if a signal is received to shutdown the crawler
 SHUTDOWN_TIMEOUT = 5
